1b69a1343f61f57e5d5331641254a9f82e518b26
d-kang <david.kang714@gmail.com>
2017-06-22 23:09:11 -0700

    On App Component: Set Initial State, addFish Method Set inital state fishes and order to empty obj addFish method will update our state First create a copy of initial state Give a unique key via Date.now()

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,30 @@ import Inventory from './Inventory';
 
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    // initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  // it should get state from child components
+  addFish(fish) {
+    // update our state (best practice is to create a copy of state and then update)
+    const fishes = { ...this.state.fishes };
+    // add in our new fish
+      // needs unique key, will use timestamp
+      const timestamp = Date.now();
+      fishes[timestamp] = 1
+      this.state.fishes = fishes;
+      console.log({fish, 'this.state.fishes': this.state.fishes})
+    //set state
+    // example of bad practice
+      // this.state.fishes.fish1 = 1;
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -13,6 +37,7 @@ export default class App extends React.Component {
         </div>
         <Order />
         <Inventory />
+        <button onClick={e => this.addFish(e)}></button>
       </div>
     )
   }
