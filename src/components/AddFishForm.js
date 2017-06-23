6f98fd4d135f433c8ca18340e38a3fa507ff9ed2
d-kang <david.kang714@gmail.com>
2017-06-22 22:39:26 -0700

    createFish Method On AddFishForm, Add Refs on Input Fields Add Function Refs to Form Fields to gain references to elements with which we can use a method createFish to create a fish object that will hold the values of each input.

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -1,19 +1,30 @@
 import React from 'react'
 
 export default class AddFishForm extends React.Component {
-
+  createFish(e) {
+    e.preventDefault();
+    console.log('Gonna make some fish!');
+    const fish = {
+      name: this.name.value,
+      price: this.price.value,
+      status: this.status.value,
+      desc: this.desc.value,
+      image: this.image.value
+    }
+    console.log({fish});
+  }
   render() {
     return (
       <div>
-        <form action="" className="fish-edit">
-          <input type="text" placeholder="Fish Name"/>
-          <input type="text" placeholder="Fish Price"/>
-          <select name="" id="">
+        <form action="" className="fish-edit" onSubmit={ e => this.createFish(e) }>
+          <input ref={ input => this.name = input } type="text" placeholder="Fish Name"/>
+          <input ref={ input => this.price = input } type="text" placeholder="Fish Price"/>
+          <select ref={ input => this.status = input } name="" id="">
             <option value="available">Fresh!</option>
             <option value="unavailable">Sold Out!</option>
           </select>
-          <textarea placeholder="FishDesc"></textarea>
-          <input type="text" placeholder="Fish Image"/>
+          <textarea ref={ input => this.desc = input } placeholder="FishDesc"></textarea>
+          <input ref={ input => this.image = input } type="text" placeholder="Fish Image"/>
           <button type="submit">+ Add Item</button>
         </form>
       </div>
