1bf21ab03d2066f3dfc06e578a0d170f49fa2c87
d-kang <david.kang714@gmail.com>
2017-06-22 23:19:11 -0700

    Add setState to addFish Method react will update that state for us everywhere that that state is living

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,27 @@ export default class App extends React.Component {
 
   // it should get state from child components
   addFish(fish) {
+    // example of bad practice
+      // this.state.fishes.fish1 = 1;
     // update our state (best practice is to create a copy of state and then update)
     const fishes = { ...this.state.fishes };
     // add in our new fish
       // needs unique key, will use timestamp
       const timestamp = Date.now();
-      fishes[timestamp] = 1
-      this.state.fishes = fishes;
+      fishes[timestamp] = fish;
+
       console.log({fish, 'this.state.fishes': this.state.fishes})
     //set state
-    // example of bad practice
-      // this.state.fishes.fish1 = 1;
+    // you could do this.state.fishes = fishes; but it is better to use setState method
+    // setState will tell React to rerender
+    // you only pass what has updated because there could be several things in your state and React doesnt want to have to rerender all of it if you already know what part of your state you want to rerender
+    // the one piece of state that has changed here is the fishes so we pass that into setState
+    // react will update that state for us everywhere that that state is living
+    this.setState({ fishes })
+    console.log({fish, 'this.state.fishes': this.state.fishes})
+
+
+
   }
 
   render() {
